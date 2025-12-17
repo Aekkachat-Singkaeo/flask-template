4b375b48dba385a844159f0dc562d4b53cd4b373
Aekkachat-Singkaeo <std.68122420120@ubru.ac.th>
2025-12-17 22:59:56 +0700

    add movie.hthml

--- server.py
+++ server.py
@@ -5,21 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
      return render_template('index.html', title='home Page')
+
 @app.route('/about')
 def about():
     title='About Page'
     name='Aekkachat'
-    email='eafafa'              
+    email='aekkachat@example.com'
     phone='0812345678'
-
-
     return render_template('about.html', title=title,  name=name, email=email, phone=phone)
 
 @app.route('/favoerite/food')
 def favorite_food():
     title='Favorite Food'
     foods = ['Pizza', 'Sushi', 'Ice Cream', 'Tacos']
-    return render_template('food.html', foods=foods,itle=title)
+    return render_template('food.html', foods=foods, title=title)
 
 @app.route('/favorite/sport')
 def favorite_sport():
@@ -31,3 +30,9 @@ def favorite_sport():
 def wlcome(username):
     title='Welcome Page'
     return render_template('welcome.html',title=title, username=username)
+
+@app.route('/square/Favorite_movie')
+def favorite_movie():
+    title='Favorite Movie'
+    movies = ['Inception', 'The Matrix', 'Interstellar', 'The Dark Knight', 'Forrest Gump']
+    return render_template('movie.html', movies=movies, title=title)
